8a3b30c5f858b6f5a6cb2b7ef5ed213a0d16bf92
Francesco-taioli <taioli.francesco98@gmail.com>
2022-01-25 11:16:00 +0100

    update code structure

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import numpy as np
 import utils.Homography
 from utils.Homography import Homography
 from utils.Enlarger import Enlarger
+import argparse
 
-def main():
-    left_camera = cv2.VideoCapture('input_videos/test.mp4')
-    right_camera = cv2.VideoCapture('input_videos/test_2.mp4')
+
+def enlarge_videos_fov(left_path, right_path):
+    left_camera = cv2.VideoCapture(left_path)
+    right_camera = cv2.VideoCapture(right_path)
     if not left_camera.isOpened() or not right_camera.isOpened():
         print("Error opening the video file")
         return
-
 
     # Get frame rate information
     print(f"Frames per second (left camera) :", left_camera.get(cv2.CAP_PROP_FPS))
@@ -25,21 +26,29 @@ def main():
 
         # compute the homography once
         if has_to_compute_homography:
-            print("Computing homography ...")
+            print("Computing homography for the first time ...")
             homography = Homography.compute_homography(frame_left, frame_left)
             has_to_compute_homography = False
 
         # combined from the left and right frame
         img_enlarged = Enlarger.enlarge_fov(frame_left, frame_left)
 
-        #todo : save image
+        # todo : save image for every frame - construct video
 
-        if not ret_right or not ret_left : break
+        if not ret_right or not ret_left: break
 
     # Release the video capture object
     left_camera.release()
     right_camera.release()
     return
 
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Video enlarger')
+    parser.add_argument('--left-video-path', type=str,
+                        help='path to the left video')
+    parser.add_argument('--right-video-path', type=str,
+                        help='path to the right video')
+    args = parser.parse_args()
+
+    enlarge_videos_fov(args.left_video_path, args.right_video_path)
