07af72a5618dcd8428523bd2550c00828b58239a
ScodroS <sebastianoscodro@gmail.com>
2022-02-17 16:56:51 +0100

    Working branch

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import cv2
 import numpy as np
-import utils.Homography
-import  utils.Homography as Homography
-import utils.Enlarger  as Enlarger
+import utils.Homography as Homography
+import utils.Enlarger as Enlarger
 import argparse
+from os.path import join as osp
+import platform
+import datetime
 from tqdm import tqdm
+from subprocess import Popen, PIPE
+from utils.syncstart import file_offset
 
-def enlarge_videos_fov(left_path, right_path):
+def enlarge_videos_fov(left_path, right_path, grayscale):
     left_camera = cv2.VideoCapture(left_path)
     right_camera = cv2.VideoCapture(right_path)
+
     if not left_camera.isOpened() or not right_camera.isOpened():
         print("Error opening the video file")
         return
@@ -20,7 +25,6 @@ def enlarge_videos_fov(left_path, right_path):
     print(f"Frames per second (right camera) :", right_camera.get(cv2.CAP_PROP_FPS))
     if not fps_right == fps_left :
         print("! NOTE !: fps are different for the cameras")
-        # todo handle the case
 
     has_to_compute_homography = True
     homography = np.zeros((3, 3))
@@ -39,9 +43,9 @@ def enlarge_videos_fov(left_path, right_path):
         ret_right, frame_right = right_camera.read()
         if not ret_right or not ret_left: break
 
-        # transform to rgb
-        # frame_left = cv2.cvtColor(frame_left, cv2.COLOR_BGR2RGB)
-        # frame_right = cv2.cvtColor(frame_right, cv2.COLOR_BGR2RGB)
+        if grayscale:
+            frame_left = cv2.cvtColor(cv2.cvtColor(frame_left, cv2.COLOR_RGB2GRAY), cv2.COLOR_GRAY2RGB)
+            frame_right = cv2.cvtColor(cv2.cvtColor(frame_right, cv2.COLOR_RGB2GRAY), cv2.COLOR_GRAY2RGB)
 
         # compute the homography once
         if has_to_compute_homography:
@@ -84,6 +88,39 @@ if __name__ == '__main__':
                         help='path to the left video')
     parser.add_argument('--right-video-path', type=str,
                         help='path to the right video')
+    parser.add_argument('--grayscale', type=int,
+                        help='generate grayscale output,'
+                             ' useful when combining videos obtained with different cameras',
+                        default=0)
     args = parser.parse_args()
 
-    enlarge_videos_fov(args.left_video_path, args.right_video_path)
+    # implementare sync video
+
+    syncargs = {'in1': args.right_video_path,
+                'in2': args.left_video_path,
+                'take': 20,
+                'show': False,
+                'normalize': False,
+                'denoise': False,
+                'lowpass': 0}
+
+    file, offset = file_offset(syncargs)
+    cutvideo = ""
+    newname = "input_videos/cutvid.mp4"
+    # taglio video
+    # ffmpeg -i 5_L.mp4  -ss 00:00:00.75 -async 1 5_L_prod.mp4
+    if file == args.right_video_path:
+        video_to_cut = args.right_video_path
+        cutvideo = "right"
+    else:
+        video_to_cut = args.left_video_path
+        cutvideo = "left"
+
+    process = Popen(['ffmpeg', '-i', video_to_cut, '-ss', str(offset), '-async', '1', newname], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    if cutvideo == "right":
+        enlarge_videos_fov(args.left_video_path, newname, args.grayscale)
+    elif cutvideo == "left":
+        enlarge_videos_fov(newname, args.right_video_path, args.grayscale)
+    else:
+        enlarge_videos_fov(args.left_video_path, args.right_video_path, args.grayscale)
